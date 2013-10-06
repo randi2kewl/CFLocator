5bf426a7e7e185400316ad5a0d19b62f3ca543b8
Andrew Nelson <werdnanoslen@risu.(none)>
2013-10-06 00:17:48 -0400

    Added better error handling

--- php/app.php
+++ php/app.php
@@ -8,17 +8,29 @@ ini_set("log_errors", 1);
 
 function getLocs()
 {
-	/*$db = new PDO('mysql:host='.HOST.';dbname='.DBNAME, USERNAME, PASSWORD);
+    try 
+    {
+        $db = new PDO(
+            'mysql:host='.HOST.';dbname='.DBNAME, 
+            USERNAME, 
+            PASSWORD,
+            array
+            (
+                PDO::ATTR_EMULATE_PREPARES => false, 
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            )
+        );
+    } 
+    catch(PDOException $e) 
+    {
+        error_log('ERROR: '.$e->getMessage());
+        return;
+    }
+    
 	$statement = $db->prepare('SELECT * FROM `log`');
 	$statement->execute();
 	error_log(print_r($statement->errorInfo(), true));
-	$result = $statement->fetchAll(PDO::FETCH_ASSOC);*/
-	$result = array( //temporary
-        array('id' => '1','device_id' => NULL,'log_entry' => '2013-10-06 01:42:09','lat' => '33.7744','lon' => '-84.3966'),
-        array('id' => '2','device_id' => NULL,'log_entry' => '2013-10-06 02:02:14','lat' => '33.7744','lon' => '-84.3964'),
-        array('id' => '3','device_id' => NULL,'log_entry' => '2013-10-06 02:22:16','lat' => '33.7744','lon' => '-84.3962'),
-        array('id' => '4','device_id' => NULL,'log_entry' => '2013-10-06 02:42:18','lat' => '33.7744','lon' => '-84.3960')
-    );
+	$result = $statement->fetchAll(PDO::FETCH_ASSOC);
     
     //Add time since reported (up to 2 hours) as int 0-9
     $time = time();
